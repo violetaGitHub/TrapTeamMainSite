342d2b7bc2050dfea5669ac2c72e6899e98abf6b
Stevie <bowyers.nz@gmail.com>
2019-05-26 18:17:09 +1200

    Update redir.js

--- redir.js
+++ redir.js
@@ -1,3 +1,3 @@
 var str = document.location.href;
-var res = str.replace("https://trapteam-cc.firebaseapp.com", "http://trapteamnz.tk");
+var res = str.replace("https://trapteam-cc.firebaseapp.com", "http://www.trapteamnz.tk");
 document.location.href = res;
