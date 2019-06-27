897be02e3c0ceefe97038121bafb4eff22fb48a3
Restyled.io <commits@restyled.io>
2019-06-27 08:02:19 +0000

    Restyled by prettier

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,19 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp()
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 exports.FillTrap = functions.https.onRequest((req, res) => {
   if (req.method === "POST") {
     var TrapHolder = req.body.owner;
     var TrapName = req.body.name;
-    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Full");
+    admin
+      .database()
+      .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+      .set("Full");
     console.log("Filled " + TrapHolder + "'s Trap.");
     res.status(200).send("Filled " + TrapHolder + "'s Trap.");
   } else {
-    console.error('Not POST: ' + req.method);
+    console.error("Not POST: " + req.method);
     res.status(405).send("Error, Must send with POST not: " + req.method);
   }
 });
@@ -19,24 +22,32 @@ exports.EmptyTrap = functions.https.onRequest((req, res) => {
   if (req.method === "POST") {
     var TrapHolder = req.body.owner;
     var TrapName = req.body.name;
-    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Empty");
+    admin
+      .database()
+      .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+      .set("Empty");
     console.log("Emptyed " + TrapHolder + "'s Trap.");
     res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
   } else {
-    console.error('Not POST: ' + req.method);
+    console.error("Not POST: " + req.method);
     res.status(405).send("Error, Must send with POST not: " + req.method);
   }
 });
 
-exports.addAccount = functions.auth.user().onCreate((user) => {
+exports.addAccount = functions.auth.user().onCreate(user => {
   if (user.email === null) {
-    user.email = user.phoneNumber
+    user.email = user.phoneNumber;
   }
-  
+
   const email = user.email; // The email of the user.
   const id = user.uid;
   const displayName = user.displayName; // The display name of the user.
-  admin.database().ref("/Trapholders/" + id + "/traps/name").set("test");
-  return admin.database().ref("/Trapholders/" + id + "/email").set(email);
-  
+  admin
+    .database()
+    .ref("/Trapholders/" + id + "/traps/name")
+    .set("test");
+  return admin
+    .database()
+    .ref("/Trapholders/" + id + "/email")
+    .set(email);
 });
