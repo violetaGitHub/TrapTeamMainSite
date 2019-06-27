f8c6f8f3bb4ce5c2417153f379c63682f6711cc9
Ezrashaw <Ezrashaw1414@gmail.com>
2019-06-27 19:58:43 +1200

    BUGFIX FBFunctions: Updated Functions
    Uodated All. Fixed bugs reported earlier today. All Working with FillTrap and EmptyTrap.

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,30 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
 admin.initializeApp()
 
-exports.ToggleTrap = functions.https.onRequest((request, response) => {
-  if (request.method === "POST") {
-    var TrapHolder = request.body.owner;
-    var TrapName = request.body.name;
-    var MainToggleVal;
-    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").once('value', (snapshot) => {
-      MainToggleVal = snapshot.val();
-    });
-    if (MainToggleVal == "Full")
-    {
-      admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Empty");
-      console.log("Emptyed " + TrapHolder + "'s Trap.");
-      console.log(MainToggleVal);
-      response.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
-    }
-    else
-    {
-      admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Full");
-      console.log("Filled " + TrapHolder + "'s Trap.");
-      console.log(MainToggleVal);
-      response.status(200).send("Filled " + TrapHolder + "'s Trap.");
-    }
+exports.FillTrap = functions.https.onRequest((req, res) => {
+  if (req.method === "POST") {
+    var TrapHolder = req.body.owner;
+    var TrapName = req.body.name;
+    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Full");
+    console.log("Filled " + TrapHolder + "'s Trap.");
+    res.status(200).send("Filled " + TrapHolder + "'s Trap.");
   } else {
-    console.error('Not POST: ' + request.method);
-    response.status(405).send("Error, Must send with POST not: " + request.method);
+    console.error('Not POST: ' + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
+  }
+});
+
+exports.EmptyTrap = functions.https.onRequest((req, res) => {
+  if (req.method === "POST") {
+    var TrapHolder = req.body.owner;
+    var TrapName = req.body.name;
+    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Empty");
+    console.log("Emptyed " + TrapHolder + "'s Trap.");
+    res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
+  } else {
+    console.error('Not POST: ' + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
   }
 });
 
