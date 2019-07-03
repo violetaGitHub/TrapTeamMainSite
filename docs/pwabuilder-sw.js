ef22023040299dfdf564603f53fa8699a8e8e072
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2019-07-03 17:33:35 +1200

    feat: add form for adding trap  (#57)
    * feature: Add better Test.js (iServe.js)
    
    * feature: app/index.html
    
    * chore docs/iServe.js: Fixed incorrect port
    
    Also added port
    
    * chore: add twixpx
    
    * [CodeFactor] Apply fixes
    
    * Restyled by prettier
    
    * Restyled by shfmt
    
    * feat: make addtrap form
    
    * bugfix: docs/test.bat
    
    Make Local Server Work
    
    * bugfix docs/app/index.html:
    
    Fixed the design and layout
    
    * feat: docs/app/index.html
    
    Add Mobile Compatibility
    
    * chore: docs/app/index.html
    
    Update jQuery
    
    * chore: update analytics across files
    
    * chore: docs/authneeded.html
    
    Fix Analytics
    
    * bugfix: docs/home.html
    
    * feat: docs/app/index.html
    
    Make Cards Responsive
    
    * feat: add jui.js
    
    * Restyled by prettier
    
    * chore: docs/app/index.html
    
    * feat docs/app/:
    
    index.html: Externalise JS, add vue.js;
    authhead.js: add js from index.html;
    authfoot.js: add js from index.html and Make Vue Go!;
    
    * Restyled by prettier
    
    * feat: docs/app/
    
    make ext FAB
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * feat: app/*
    
    Make the app verify your email

--- docs/pwabuilder-sw.js
+++ docs/pwabuilder-sw.js
@@ -1,43 +1,42 @@
-const CACHE = "pwabuilder-offline-pages";
+const CACHE = "pwabuilder-offline-pagesss";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "index.html";
 
 // Install stage sets up the offline page in the cache and opens a new cache
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   console.log("[PWA Builder] Install Event processing");
-  
+
   event.waitUntil(
-    caches.open(CACHE).then((cache) => {
+    caches.open(CACHE).then(cache => {
       console.log("[PWA Builder] Cached offline page during install");
-      
-      if (offlineFallbackPage === "blahblah.html") {
-        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-      }
-      
+
       return cache.add(offlineFallbackPage);
     })
   );
 });
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  
+
   event.respondWith(
     fetch(event.request)
-    .then((response) => {
-      console.log("[PWA Builder] add page to offline cache: " + response.url);
-      
-      // If request was success, add or update it in the cache
-      event.waitUntil(updateCache(event.request, response.clone()));
-      
-      return response;
-    })
-    .catch((error) => {
-      console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
-      return fromCache(event.request);
-    })
+      .then(response => {
+        console.log("[PWA Builder] add page to offline cache: " + response.url);
+
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+
+        return response;
+      })
+      .catch(error => {
+        console.log(
+          "[PWA Builder] Network request Failed. Serving content from cache: " +
+            error
+        );
+        return fromCache(event.request);
+      })
   );
 });
 
@@ -45,24 +44,24 @@ function fromCache(request) {
   // Check to see if you have it in the cache
   // Return response
   // If not in the cache, then return the offline page
-  return caches.open(CACHE).then((cache) => {
-    return cache.match(request).then((matching) => {
+  return caches.open(CACHE).then(cache => {
+    return cache.match(request).then(matching => {
       if (!matching || matching.status === 404) {
         // The following validates that the request was for a navigation to a new document
         if (request.destination !== "document" || request.mode !== "navigate") {
           return Promise.reject("no-match");
         }
-        
+
         return cache.match(offlineFallbackPage);
       }
-      
+
       return matching;
     });
   });
 }
 
 function updateCache(request, response) {
-  return caches.open(CACHE).then((cache) => {
+  return caches.open(CACHE).then(cache => {
     return cache.put(request, response);
   });
 }
