746d88deb0a6fc5c179b55c9054fbace32213afd
Ezrashaw <Ezrashaw1414@gmail.com>
2019-07-16 10:43:28 +1200

    fix Index.js: Removed Merge Markers
    Also fixed old/outdated/corrupt-functions-from-merge/stuff-that's-not-working.

--- functions/index.js
+++ functions/index.js
@@ -7,36 +7,6 @@ admin.initializeApp({
   databaseURL: "https://trapteam-cc.firebaseio.com"
 });
 
-exports.FillTrap = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    console.error("Not POST: " + req.method);
-    res.status(405).send("Error, Must send with POST not: " + req.method);
-  }
-  var TrapHolder = req.body.owner;
-  var TrapName = req.body.name;
-  admin
-    .database()
-    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
-    .set("Full");
-  console.log("Filled " + TrapHolder + "'s Trap.");
-  res.status(200).send("Filled " + TrapHolder + "'s Trap.");
-});
-
-exports.EmptyTrap = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    console.error("Not POST: " + req.method);
-    res.status(405).send("Error, Must send with POST not: " + req.method);
-  }
-  var TrapHolder = req.body.owner;
-  var TrapName = req.body.name;
-  admin
-    .database()
-    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
-    .set("Empty");
-  console.log("Emptyed " + TrapHolder + "'s Trap.");
-  res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
-});
-
 exports.ToggleTrap = functions.https.onRequest((req, res) => {
   if (req.method !== "POST") {
     console.error("Not POST: " + req.method);
@@ -67,6 +37,36 @@ exports.ToggleTrap = functions.https.onRequest((req, res) => {
         res.status(400).send("You mucked up!!!");
       }
     });
+});
+
+exports.FillTrap = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    console.error("Not POST: " + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
+  }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin
+    .database()
+    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+    .set("Full");
+  console.log("Filled " + TrapHolder + "'s Trap.");
+  res.status(200).send("Filled " + TrapHolder + "'s Trap.");
+});
+
+exports.EmptyTrap = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    console.error("Not POST: " + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
+  }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin
+    .database()
+    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+    .set("Empty");
+  console.log("Emptyed " + TrapHolder + "'s Trap.");
+  res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
 });
 
 exports.GetTrapNumber = functions.https.onRequest((req, res) => {
@@ -111,94 +111,17 @@ exports.AddTrap = functions.https.onRequest((req, res) => {
     .set(TrapNumber);
   res.status(200).send("Working, Completed NewTrap Operation.");
 });
-
+  
 exports.addAccount = functions.auth.user().onCreate(user => {
   if (user.email === null) {
     user.email = user.phoneNumber;
   }
-
+  
   const email = user.email; // The email of the user.
   const id = user.uid;
   const displayName = user.displayName; // The display name of the user.
-  admin
-    .database()
-    .ref("/Trapholders/" + id + "/traps/name")
-    .set("test");
   return admin
-    .database()
-    .ref("/Trapholders/" + id + "/email")
-    .set(email);
+  .database()
+  .ref("/Trapholders/" + id + "/email")
+  .set(email);
 });
-=======
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-exports.FillTrap = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    console.error('Not POST: ' + req.method);
-    if (req.method === "POST") {
-      var TrapHolder = req.body.owner;
-      var TrapName = req.body.name;
-      admin
-      .database()
-      .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
-      .set("Full");
-      console.log("Filled " + TrapHolder + "'s Trap.");
-      res.status(200).send("Filled " + TrapHolder + "'s Trap.");
-    } else {
-      console.error("Not POST: " + req.method);
-      res.status(405).send("Error, Must send with POST not: " + req.method);
-    }
-    var TrapHolder = req.body.owner;
-    var TrapName = req.body.name;
-    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Full");
-    console.log("Filled " + TrapHolder + "'s Trap.");
-    res.status(200).send("Filled " + TrapHolder + "'s Trap.");
-  });
-  
-  exports.EmptyTrap = functions.https.onRequest((req, res) => {
-    if (req.method !== "POST") {
-      console.error('Not POST: ' + req.method);
-      res.status(405).send("Error, Must send with POST not: " + req.method);
-    }
-    var TrapHolder = req.body.owner;
-    var TrapName = req.body.name;
-    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Empty");
-    console.log("Emptyed " + TrapHolder + "'s Trap.");
-    res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
-  });
-  
-  exports.EmptyTrap = functions.https.onRequest((req, res) => {
-    if (req.method === "POST") {
-      var TrapHolder = req.body.owner;
-      var TrapName = req.body.name;
-      admin
-      .database()
-      .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
-      .set("Empty");
-      console.log("Emptyed " + TrapHolder + "'s Trap.");
-      res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
-    } else {
-      console.error("Not POST: " + req.method);
-      res.status(405).send("Error, Must send with POST not: " + req.method);
-    }
-  });
-  
-  exports.addAccount = functions.auth.user().onCreate(user => {
-    if (user.email === null) {
-      user.email = user.phoneNumber;
-    }
-    
-    const email = user.email; // The email of the user.
-    const id = user.uid;
-    const displayName = user.displayName; // The display name of the user.
-    admin
-    .database()
-    .ref("/Trapholders/" + id + "/traps/name")
-    .set("test");
-    return admin
-    .database()
-    .ref("/Trapholders/" + id + "/email")
-    .set(email);
-  });
