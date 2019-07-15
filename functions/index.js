8da21ac844679de41438d40c8714eb040fac6741
Ezrashaw <Ezrashaw1414@gmail.com>
2019-07-16 09:37:42 +1200

    feat: add toggle trap function PR WIP (#54)
    * BUGFIX FBFunctions: Updated Functions
    
    Uodated All. Fixed bugs reported earlier today. All Working with FillTrap and EmptyTrap.
    
    * feat FBFunctions: Added ToggleTrap Function
    
    First Version of ToggleTrap
    
    * Restyled by prettier
    
    * fix Index.js: lint check
    
    lint check failed. Updated to match standards.
    
    * Restyled by prettier
    
    * chore .gitignore: added ServiceAccountKey.json to list
    
    * chore: functions/index.js
    
    Lint Fix
    
    * feat index.js: Created CreateUser Function
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * fix Index.js: Fixed Non-working code
    
    Fixed code.
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * feat: Add trap function (#108)
    
    * feat Index.js: Added 2 new functions
    
    * feat Index.js: Added AddTrap Function
    
    feat tested. All working. Ready for new version and merge to master
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * Restyle Add trap function (#109)
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace
    
    * Restyled by prettier
    
    * CodeTrimmer.app Bot: Trim whitespace

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,135 @@
+var functions = require("firebase-functions");
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://trapteam-cc.firebaseio.com"
+});
+
+exports.FillTrap = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    console.error("Not POST: " + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
+  }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin
+    .database()
+    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+    .set("Full");
+  console.log("Filled " + TrapHolder + "'s Trap.");
+  res.status(200).send("Filled " + TrapHolder + "'s Trap.");
+});
+
+exports.EmptyTrap = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    console.error("Not POST: " + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
+  }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin
+    .database()
+    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+    .set("Empty");
+  console.log("Emptyed " + TrapHolder + "'s Trap.");
+  res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
+});
+
+exports.ToggleTrap = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    console.error("Not POST: " + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
+  }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin
+    .database()
+    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+    .once("value", snapshot => {
+      var Value = snapshot.val();
+      if (Value == "Full") {
+        admin
+          .database()
+          .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+          .set("Empty");
+        console.log("Emptyed " + TrapHolder + "'s Trap.");
+        res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
+      } else if (Value === "Empty") {
+        admin
+          .database()
+          .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+          .set("Full");
+        console.log("Filled " + TrapHolder + "'s Trap.");
+        res.status(200).send("Filled " + TrapHolder + "'s Trap.");
+      } else {
+        res.status(400).send("You mucked up!!!");
+      }
+    });
+});
+
+exports.GetTrapNumber = functions.https.onRequest((req, res) => {
+  if (req.method !== "GET") {
+    console.error("Not GET: " + req.method);
+    res.status(405).send("Error, Must send with GET not: " + req.method);
+  }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin
+    .database()
+    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/ID")
+    .once("value", snapshot => {
+      var Value = snapshot.val();
+      res
+        .status(200)
+        .send(
+          TrapName +
+            " is owned by " +
+            TrapHolder +
+            " and has Trap Number of " +
+            value
+        );
+    });
+});
+
+exports.AddTrap = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    console.error("Not POST: " + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
+  }
+  var TrapNumber = req.body.number;
+  var TrapName = req.body.name;
+  var TrapHolder = req.body.TrapHolder;
+  admin
+    .database()
+    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status")
+    .set("Empty");
+  admin
+    .database()
+    .ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/ID")
+    .set(TrapNumber);
+  res.status(200).send("Working, Completed NewTrap Operation.");
+});
+
+exports.addAccount = functions.auth.user().onCreate(user => {
+  if (user.email === null) {
+    user.email = user.phoneNumber;
+  }
+
+  const email = user.email; // The email of the user.
+  const id = user.uid;
+  const displayName = user.displayName; // The display name of the user.
+  admin
+    .database()
+    .ref("/Trapholders/" + id + "/traps/name")
+    .set("test");
+  return admin
+    .database()
+    .ref("/Trapholders/" + id + "/email")
+    .set(email);
+});
+=======
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
@@ -70,4 +202,3 @@ exports.FillTrap = functions.https.onRequest((req, res) => {
     .ref("/Trapholders/" + id + "/email")
     .set(email);
   });
-  
