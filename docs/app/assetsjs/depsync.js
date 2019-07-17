932af186a93647422ca85c7b2880d6c7706a83d2
Steviebeenz <bowyers.nz@gmail.com>
2019-07-18 10:40:51 +1200

    Update depsync.js

--- docs/app/assetsjs/depsync.js
+++ docs/app/assetsjs/depsync.js
@@ -13,3 +13,16 @@ LogRocket.getSessionURL(sessionURL => {
     scope.setExtra("sessionURL", sessionURL);
   });
 });
+var firebaseConfig = {
+    apiKey: "AIzaSyBHB7H6L2TgZeDJKS4ENC_fkdWtS_99T_E",
+    authDomain: "trapteam-cc.firebaseapp.com",
+    databaseURL: "https://trapteam-cc.firebaseio.com",
+    projectId: "trapteam-cc",
+    storageBucket: "trapteam-cc.appspot.com",
+    messagingSenderId: "150993996289",
+    appId: "1:150993996289:web:9a6a477accc058af"
+  };
+(function(sa,fbc){function load(f,c){var a=document.createElement('script');
+a.async=1;a.src=f;var s=document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(a,s);}load(sa);window.onload = function() {firebase.initializeApp(fbc).performance();};
+})(performance_standalone, firebaseConfig);
