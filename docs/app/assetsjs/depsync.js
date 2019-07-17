2fe62c20e4db1331abfd24a8876765dea9449805
Steviebeenz <bowyers.nz@gmail.com>
2019-07-18 10:27:22 +1200

    some: thing

--- docs/app/assetsjs/depsync.js
+++ docs/app/assetsjs/depsync.js
@@ -1,9 +1,5 @@
 window.LogRocket && window.LogRocket.init("nipfpt/trap-team-cc");
 
-window.bugsnag.beforeNotify = function(data) {
-  data.metaData.sessionURL = LogRocket.sessionURL;
-  return data;
-};
 LogRocket.getSessionURL((sessionURL) => {
   analytics.track("LogRocket", {
     sessionURL: sessionURL
--- docs/iServe.js
+++ docs/iServe.js
@@ -1,7 +1,5 @@
-var connect = require("connect");
-var serveStatic = require("serve-static");
-connect()
-  .use(serveStatic(__dirname))
-  .listen(80, () => {
-    console.log("Server running on port 80...");
-  });
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
