f393d4a092421b0238ab265921aee095f50d7eed
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2019-08-25 08:47:41 +1200

    Update git.js

--- git.js
+++ git.js
@@ -1,3 +1,10 @@
 var shell = require('shelljs');
 shell.exec("git remote add githubdirry https://github.com/TrapTeamCCNZ/TrapTeamMainSite.git");
 shell.exec("git remote add gitlabby https://gitlab.com/trapteamccnz/TrapTeamMainSite.git");
+shell.exec("npm install");
+shell.exec("npm i -g yarn");
+shell.exec("yarn install");
+shell.exec("yarn add shelljs");
+shell.exec("npm i -g firebase-tools");
+shell.exec("yarn install");
+shell.exec("firebase serve");
