c1e933198df5a8b1bf81cb66df74040c7eb46e67
Steviebeenz <40313430+steviebeenz@users.noreply.github.com>
2019-08-25 08:43:41 +1200

    Update git.js

--- git.js
+++ git.js
@@ -1,2 +1,3 @@
 var shell = require('shelljs');
-shell.exec();
+shell.exec("git remote add githubdirry https://github.com/TrapTeamCCNZ/TrapTeamMainSite.git");
+shell.exec("git remote add gitlabby https://gitlab.com/trapteamccnz/TrapTeamMainSite.git");
