138da5dd29f061b6b98687d60c08ad2d72b26b6a
seviebeenz <bowyers.nz@gmail.com>
2019-08-26 16:23:30 +1200

    KOMMIT

--- git.js
+++ git.js
@@ -1,17 +1,33 @@
 var shell = require('shelljs');
 shell.exec("git remote add githubdirry https://github.com/TrapTeamCCNZ/TrapTeamMainSite.git");
 shell.exec("git remote add gitlabby https://gitlab.com/trapteamccnz/TrapTeamMainSite.git");
-shell.exec("git stash stasha");
-shell.exec("git unstash stasha");
+shell.exec("dir");
+shell.exec("git add -A");
+shell.exec("git stash");
+shell.exec("git pull githubdirry");
+shell.exec("git pull gitlabby");
+shell.exec("git commit --all");
+shell.exec("git push githubdirry");
+shell.exec("git stash apply");
+console.log("DONE");
 shell.exec("npm install git-lab-cli -g");
 shell.exec("npm install");
 shell.exec("npm i -g yarn");
-shell.exec("npm i -g choco");
-shell.exec("npm install -g concurrently");
-shell.exec("yarn add shelljs");
+try {
+    var foo = require('choco')
+} catch (er) {
+    foo = null
+    shell.exec("npm i -g choco");
+
+}
+console.log("DONE");
+shell.exec("yarn install");
 shell.exec("yarn add choco");
+console.log("DONE");
 shell.exec("npm i -g firebase-tools");
 shell.exec("yarn install");
 shell.exec("choco install hub");
+console.log("DONE");
 shell.exec("lab merge-request");
 shell.exec("firebase serve");
+console.log("DONE");
