d30a92b6b72e79429b892593e268367687e6c4d1
Steviebeenz <bowyer.s.nz@gmail.com>
2019-09-08 16:53:16 +1200

    Deploy!

--- docs/goture.js
+++ docs/goture.js
@@ -1,3 +1,16 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBHB7H6L2TgZeDJKS4ENC_fkdWtS_99T_E",
+    authDomain: "trapteam-cc.firebaseapp.com",
+    databaseURL: "https://trapteam-cc.firebaseio.com",
+    projectId: "trapteam-cc",
+    storageBucket: "trapteam-cc.appspot.com",
+    messagingSenderId: "150993996289",
+    appId: "1:150993996289:web:9a6a477accc058af"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 // Instantiate the GoTrue auth client with an optional configuration
 
 auth = new GoTrue({
@@ -7,7 +20,13 @@ auth = new GoTrue({
 function loggy() {
     try {
         
-firebase.auth().createUserWithEmailAndPassword(auth.currentUser().email, auth.currentUser().token.access_token).catch(function(error) {
+firebase.auth().createUserWithEmailAndPassword(auth.currentUser().email, auth.currentUser().created_at).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+firebase.auth().signInWithEmailAndPassword(auth.currentUser().email, auth.currentUser().created_at).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
